61c7d8bb2415c72f0d0dbef5788e32a747260f3b
Arnab-R0Y <arnab2507@gmail.com>
2025-08-09 20:23:32 +0530

    Made the website responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  if (mobileMenu.style.display === 'flex') {
+    mobileMenu.style.display = 'none';
+  } else {
+    mobileMenu.style.display = 'flex';
+  }
+});
